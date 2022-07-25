926e446cfabce6f46be802728b91683d20da8efb
alexmercer <alex.mercer6@hotmail.com>
2022-07-25 22:47:03 +1000

    began schema

---
+++ server/db/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS materials;
+DROP TABLE IF EXISTS job;
+DROP TABLE IF EXISTS builders;
+DROP TABLE IF EXISTS trades;
+
+CREATE TABLE builders (
+    builder_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password_hash VARCHAR(255)
+);
+
+CREATE TABLE trades (
+    trade_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password_hash VARCHAR(255)
+);
+
+CREATE TABLE job (
+    job_id SERIAL PRIMARY KEY,
+    address VARCHAR(255)
+);
+
+CREATE TABLE materials (
+    material_id SERIAL PRIMARY KEY,
+    job_id REFERENCES job(job_id)
+);
