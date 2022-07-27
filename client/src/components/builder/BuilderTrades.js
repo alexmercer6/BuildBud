d7bc06368e4c7a8d9701fa00992a9250708d8fc8
alexmercer <alex.mercer6@hotmail.com>
2022-07-27 11:12:05 +1000

    finished adding some ui elements to builders tradie dahsboard

--- client/src/components/builder/BuilderTrades.js
+++ client/src/components/builder/BuilderTrades.js
@@ -5,6 +5,8 @@ import CarpenterIcon from "@mui/icons-material/Carpenter"
 import ElectricalServicesIcon from "@mui/icons-material/ElectricalServices"
 import GridViewIcon from "@mui/icons-material/GridView"
 import Crop75Icon from "@mui/icons-material/Crop75"
+import LocalShippingIcon from "@mui/icons-material/LocalShipping"
+import ArchitectureIcon from "@mui/icons-material/Architecture"
 function BuilderTrades() {
     const buttonStyle = {
         height: "100px",
@@ -20,6 +22,10 @@ function BuilderTrades() {
                 <Button alt="carpenter" variant="contained" style={buttonStyle}>
                     <CarpenterIcon />
                     Carpenter
+                </Button>
+                <Button alt="concretor" variant="contained" style={buttonStyle}>
+                    <LocalShippingIcon />
+                    Concretor
                 </Button>
                 <Button
                     alt="electrician"
@@ -40,6 +46,10 @@ function BuilderTrades() {
                 <Button alt="plumber" variant="contained" style={buttonStyle}>
                     <Crop75Icon />
                     Bricklayer
+                </Button>
+                <Button alt="Surveyor" variant="contained" style={buttonStyle}>
+                    <ArchitectureIcon />
+                    Surveyor
                 </Button>
             </div>
         </div>
