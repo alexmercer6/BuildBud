88129811f8f81118229a04447c58d46cc6eb4e87
alexmercer <alex.mercer6@hotmail.com>
2022-07-28 20:54:04 +1000

    set up add jobs api and added modal

--- client/src/components/builder/Jobs.js
+++ client/src/components/builder/Jobs.js
@@ -1,8 +1,21 @@
 import React from "react"
 import "./Builder.css"
-import { TextField, List, ListItemText, ListItemButton } from "@mui/material"
+import {
+    TextField,
+    List,
+    ListItemText,
+    ListItemButton,
+    Button,
+    Modal,
+} from "@mui/material"
+// import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material"
 import DeleteIcon from "@mui/icons-material/Delete"
 import HouseIcon from "@mui/icons-material/House"
+import AddBusinessIcon from "@mui/icons-material/AddBusiness"
+
+import axios from "axios"
+import { useState } from "react"
+import { Box, flexbox } from "@mui/system"
 
 // const data = [
 //     "Riverstone, Lot 112",
@@ -14,6 +27,7 @@ import HouseIcon from "@mui/icons-material/House"
 function Jobs({ data }) {
     return (
         <div>
+            <AddJobs />
             <TextField
                 id="standard-basic"
                 label="Search Jobs"
@@ -33,6 +47,97 @@ function Jobs({ data }) {
                     )
                 })}
             </div>
+        </div>
+    )
+}
+
+function AddJobs() {
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "white",
+        // border: "2px solid primary",
+        boxShadow: 24,
+        p: 4,
+    }
+
+    const [open, setOpen] = useState(false)
+
+    const [formInput, setFormInput] = useState({})
+
+    const handleOpen = () => setOpen(true)
+
+    const handleClose = () => setOpen(false)
+
+    const handleInput = (event) => {
+        const name = event.target.name
+        const newValue = event.target.value
+
+        setFormInput({ ...formInput, [name]: newValue })
+        console.log(formInput)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(formInput)
+    }
+    return (
+        <div>
+            <Button onClick={handleOpen}>
+                <AddBusinessIcon />
+            </Button>
+            <Modal open={open} onClose={handleClose}>
+                <Box sx={style} className="add-job-modal">
+                    <form>
+                        <TextField
+                            required
+                            id="standard-required"
+                            name="lotNumber"
+                            label="Lot Number"
+                            variant="standard"
+                            onChange={handleInput}
+                        />
+                        <TextField
+                            required
+                            label="Street Name"
+                            name="streetName"
+                            variant="standard"
+                            onChange={handleInput}
+                        />
+                        <TextField
+                            required
+                            label="Suburb"
+                            name="suburb"
+                            variant="standard"
+                            onChange={handleInput}
+                        />
+                        <TextField
+                            required
+                            label="City"
+                            name="city"
+                            variant="standard"
+                            onChange={handleInput}
+                        />
+                        <TextField
+                            required
+                            label="Postcode"
+                            name="postcode"
+                            variant="standard"
+                            onChange={handleInput}
+                        />
+                        <Button
+                            onClick={handleSubmit}
+                            type="submit"
+                            variant="contained"
+                        >
+                            Add Job
+                        </Button>
+                    </form>
+                </Box>
+            </Modal>
         </div>
     )
 }
